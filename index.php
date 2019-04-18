b33cba4a7e262f8700c0eb11802165fae44a1728
Aron Bergman <bathingrad@gmail.com>
2019-04-18 10:16:03 +0200

    Add aria-label attribute to links with svg

--- index.php
+++ index.php
@@ -11,14 +11,14 @@
 	</head>
 	<body>
 		<header>
-			<a id="logo" href="/">
+			<a id="logo" href="/" aria-label="Aron Bergman">
 				<?php include($_SERVER["DOCUMENT_ROOT"] . "/img/logo.svg")?>
 			</a>
 			<nav id="social">
-				<a href="https://github.com/WeeRox/"><?php include($_SERVER["DOCUMENT_ROOT"] . "/img/social/github.svg")?></a>
-				<a href="https://www.linkedin.com/in/aronbergman/"><?php include($_SERVER["DOCUMENT_ROOT"] . "/img/social/linkedin.svg")?></a>
-				<a href="https://twitter.com/WeeRoxs"><?php include($_SERVER["DOCUMENT_ROOT"] . "/img/social/twitter.svg")?></a>
-				<a href="https://www.facebook.com/aronbergman00"><?php include($_SERVER["DOCUMENT_ROOT"] . "/img/social/facebook.svg")?></a>
+				<a href="https://github.com/WeeRox/" aria-label="GitHub"><?php include($_SERVER["DOCUMENT_ROOT"] . "/img/social/github.svg")?></a>
+				<a href="https://www.linkedin.com/in/aronbergman/" aria-label="LinkedIn"><?php include($_SERVER["DOCUMENT_ROOT"] . "/img/social/linkedin.svg")?></a>
+				<a href="https://twitter.com/WeeRoxs" aria-label="Twitter"><?php include($_SERVER["DOCUMENT_ROOT"] . "/img/social/twitter.svg")?></a>
+				<a href="https://www.facebook.com/aronbergman00" aria-label="Facebook"><?php include($_SERVER["DOCUMENT_ROOT"] . "/img/social/facebook.svg")?></a>
 			</nav>
 		</header>
 		<main>
