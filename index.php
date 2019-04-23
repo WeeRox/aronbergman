64fff2ff09dde34daee57520f899d18e68a7cdad
Aron Bergman <bathingrad@gmail.com>
2019-04-23 12:28:30 +0200

    Extract header file and include with PHP

--- index.php
+++ index.php
@@ -10,17 +10,7 @@
 		<link rel="stylesheet" href="/css/index.css" type="text/css">
 	</head>
 	<body>
-		<header>
-			<a id="logo" href="/" aria-label="Aron Bergman">
-				<?php include($_SERVER["DOCUMENT_ROOT"] . "/img/logo.svg")?>
-			</a>
-			<nav id="social">
-				<a href="https://github.com/WeeRox/" aria-label="GitHub"><?php include($_SERVER["DOCUMENT_ROOT"] . "/img/social/github.svg")?></a>
-				<a href="https://www.linkedin.com/in/aronbergman/" aria-label="LinkedIn"><?php include($_SERVER["DOCUMENT_ROOT"] . "/img/social/linkedin.svg")?></a>
-				<a href="https://twitter.com/WeeRoxs" aria-label="Twitter"><?php include($_SERVER["DOCUMENT_ROOT"] . "/img/social/twitter.svg")?></a>
-				<a href="https://www.facebook.com/aronbergman00" aria-label="Facebook"><?php include($_SERVER["DOCUMENT_ROOT"] . "/img/social/facebook.svg")?></a>
-			</nav>
-		</header>
+		<?php include($_SERVER['DOCUMENT_ROOT'] . "/header.php"); ?>
 		<main>
 			<div class="card contact">
 				<h2>Contact</h2>
