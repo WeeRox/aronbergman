5b4c2a3d1616282850806becbed49755cc47a936
Aron Bergman <bathingrad@gmail.com>
2019-04-23 12:51:00 +0200

    Add page for posts list

--- header.php
+++ header.php
@@ -2,6 +2,11 @@
   <a id="logo" href="/" aria-label="Aron Bergman">
     <?php include($_SERVER["DOCUMENT_ROOT"] . "/img/logo.svg")?>
   </a>
+  <nav id="nav">
+    <ul>
+      <li><a href="/posts">Posts</a></li>
+    </ul>
+  </nav>
   <nav id="social">
     <a href="https://github.com/WeeRox/" aria-label="GitHub"><?php include($_SERVER["DOCUMENT_ROOT"] . "/img/social/github.svg")?></a>
     <a href="https://www.linkedin.com/in/aronbergman/" aria-label="LinkedIn"><?php include($_SERVER["DOCUMENT_ROOT"] . "/img/social/linkedin.svg")?></a>
---
+++ posts.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="theme-color" content="#0047AB">
+		<title>Posts</title>
+		<link rel="icon" href="/favicon.ico" type="image/x-icon">
+		<link rel="stylesheet" href="/css/main.css" type="text/css">
+		<link rel="stylesheet" href="/css/posts.css" type="text/css">
+	</head>
+	<body>
+		<?php include($_SERVER['DOCUMENT_ROOT'] . "/header.php"); ?>
+		<main>
+			<h1>Posts</h1>
+		</main>
+	</body>
+</html>
